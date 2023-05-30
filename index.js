9e0843f0025185d965a9b8f0f606d0a512626ec8
walidgue <75200857+walidgue@users.noreply.github.com>
2023-05-30 16:53:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 let startTime;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/src'));
+  res.sendFile(path.join(__dirname, '/src/index.html'));
 });
 
 io.on('connection', (socket) => {
