2a8fc72f86cdba70b0f20aa7e2f258530629de72
walidgue <75200857+walidgue@users.noreply.github.com>
2023-05-31 18:40:48 +0100

    public

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = 3000;
 let startTime;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, './public'));
 });
 
 io.on('connection', (socket) => {
